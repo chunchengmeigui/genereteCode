27d8ca5c5aaa6f115d379c6969b0a0513dd194a4
wdabao <723157193@qq.com>
2019-09-26 20:54:55 +0800

    更新

--- src/main/java/com/dabao/databaseUtil/Demo.java
+++ src/main/java/com/dabao/databaseUtil/Demo.java
@@ -1,5 +1,6 @@
 package com.dabao.databaseUtil;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,31 +12,38 @@ public class Demo {
 	public static void main(String[] args) {
 		//数据库配置
 		DataConnect.setDRIVER("com.mysql.cj.jdbc.Driver");
-
+new Date();
 		
-		DataConnect.setURL("jdbc:mysql://127.0.0.1:3306/dataSource1?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&useSSL=true&allowMultiQueries=true");
-		DataConnect.setUSERNAME("root");
-		DataConnect.setPASSWORD("123456");
+//		DataConnect.setURL("jdbc:mysql://127.0.0.1:3306/dataSource1?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&useSSL=true&allowMultiQueries=true");
+//		DataConnect.setUSERNAME("root");
+//		DataConnect.setPASSWORD("123456");
+		
+
 		//需要文件相关配置
 		Project.setController(true);
+		
 		Project.setService(true);
 		Project.setDoc(true);
 		Project.setEntity(true);
 		Project.setDao(true);
 		Project.setMybatis(true);
+		
+		//是否采用下划线转驼峰式
+		Project.setTransforFiled(false);
+		
 	    //是否为注解开发
-		Project.setNoteDevelop(true);
+//		Project.setNoteDevelop(true);
 		//参数为实体类
-		Project.setEntityDevelop(true);
+//		Project.setEntityDevelop(true);
 		
 		//创建表的设置
-		String	url = "E://顾客";
+		String	url = "E://数据";
 		
-		String	pakegeName = "com.dabao.mybatis";
+		String	pakegeName = "com.fineone.agriculturePc";
 //		String	pakegeName = "com.fineone.Pasteup";
 		Map<String, String> tableMap = new HashMap<String, String>();
 		//表名（key）类名（value）
-    	tableMap.put("ylf_sys_custom", "SysCustom"+","+"顾客表");
+    	tableMap.put("ylf_source_data_gather", "SourceDataGather"+","+"=编码表");
     	
 		Create createStart = new Create();
 		
